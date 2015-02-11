757799b3e0e5b1b614a7e3da4871a8281a960ba1
Jamil <jzaman@cellossoftware.com>
2015-02-11 11:01:42 +1100

    Rvalue ref

---
+++ rvalue_ref/rvalue_ref.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : rvalue_ref.cpp
+// Author      : Jamil Anwar Zama
+// Version     :
+// Copyright   : Copyright Jamil Anwar Zaman
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "RValue Reference" << endl; // prints RValue Reference
+	return 0;
+}
