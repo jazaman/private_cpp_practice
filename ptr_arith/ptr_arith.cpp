9c273e684bdb9c0923cf62c2fe26926673d04685
Jamil Anwar Zaman <jazaman8@hotmail.com>
2016-01-18 11:35:55 +0600

    delete binary and update source

--- ptr_arith/ptr_arith.cpp
+++ ptr_arith/ptr_arith.cpp
@@ -4,13 +4,13 @@
 
 void* global_ptr = 0;
 
-void destroy(void** p = 0)
+void local_swap(void** p = 0)
 {
     printf("1.swapping p = %p with %p\n", *p, global_ptr);
     std::swap(*p, global_ptr);
 }
 
-void destroy(void* p = 0)
+void local_swap(void* p = 0)
 {
     printf("2.swapping p = %p with %p\n", p, global_ptr);
     std::swap(p, global_ptr);
@@ -64,14 +64,14 @@ int main()
     int *p_int = new int[10];
     printf("Initial address for p_int:%p\n", (void*)p_int);
     printf("Initial address for global_ptr:%p\n", global_ptr);
-    destroy((void**)&p_int);
+    local_swap((void**)&p_int);
     printf("Intermediate address for global_ptr:%p\n", global_ptr);
     printf("Intermediate address for p_int:%p\n", (void*)p_int);
-    destroy((void*)p_int);
+    local_swap((void*)p_int);
     printf("Final address for global_ptr:%p\n", global_ptr);
     printf("Final address for p_int:%p\n", (void*)p_int);
 
-    std::cout << "Hello World!!" << std::endl;
+    std::cout << "Now testing shred ptr architect !!" << std::endl;
 
     std::shared_ptr<wrapper<int>> ptr_wrapper1 = std::make_shared<wrapper<int>>(99); 
     std::shared_ptr<wrapper<int>> ptr_wrapper2 = ptr_wrapper1;
