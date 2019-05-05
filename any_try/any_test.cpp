101b5a98a5d59aa9255a813ace97fe684c7cbfad
jamilz <jazaman8@yahoo.com>
2019-05-05 11:20:57 +0600

    Test any with different data type

--- any_try/any_test.cpp
+++ any_try/any_test.cpp
@@ -1,16 +1,25 @@
 #include "any.h"
 #include <iostream>
+#include <vector>
 
 
 
 int main()
 {
     int a= 5;
+    double b = 5.5;
+    char c = 'c';
 
     Any any(a);
+    std::vector<Any> anything;
+
+    anything.push_back(any);
+    anything.push_back(Any(b));
+    anything.push_back(Any(c));
     
     std::cout << "Hello World!!" << std::endl;
-    std::cout << any_cast<decltype(a)>(any) << std::endl;
-    
+    for(Any any: anything) {
+       std::cout << any_cast<decltype(any)>(any) << std::endl;
+    } 
     return 0;
 }
