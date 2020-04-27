8d62498a8a5f91240ba59634128cec92cc4f760a
jamilz <jazaman8@yahoo.com>
2020-04-28 00:21:51 +0600

    tweaking sample server options

--- client_server/sample_server.cpp
+++ client_server/sample_server.cpp
@@ -12,14 +12,17 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <arpa/inet.h>
+
+
 
 int main (int argc,char *argv[])
 {
     struct sockaddr_in p;
-    struct sockaddr_in q;
+    struct sockaddr_in clntAddr;
     //int z,s,t,x,n,b = sizeof(q);
-    int s,t,n,b = sizeof(q);
-    char buf[23];
+    int s,t,n,b = sizeof(clntAddr);
+    char buf[1024];
     int fd;
 
     s = socket(AF_INET,SOCK_STREAM,0);
@@ -29,16 +32,29 @@ int main (int argc,char *argv[])
 
     p.sin_family = AF_INET;
     p.sin_addr.s_addr = INADDR_ANY;
-    p.sin_port = htons(8080);
+    p.sin_port = htons(1234);
 
     bind (s,(struct sockaddr*)&p,sizeof(p));
     listen(s,13); //wait
-    t = accept (s,(struct sockaddr*)&q, &b);
+    t = accept (s,(struct sockaddr*)&clntAddr, &b);
+
+    char clntIP[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, (struct inaddr *)&clntAddr.sin_addr,clntIP,sizeof(clntIP));
+    printf("client IP is %s\n",clntIP);
+    int count = 10;
     while (1) {
         memset(buf, '\0', 23);
-        n = read(t,buf,1);
+        n = read(t,buf,23);
+        if(n > 0) { //read 0 byte
+            printf("Read %d byte %s sent: ",n,buf);
+                    write (t,buf,n);
+                    count = 10;
+        } else {
+            if(--count < 0)
+                break;
+        }
         //buf[n] = 0;
-        printf("Read %d byte %s sent: ",n,buf);
+
         //fd = open (buf,O_RDONLY);
         //z = lseek(fd,0,SEEK_END);
         //lseek(fd,0,SEEK_SET);
